474af9e65620d4fc7575d3c690ea9a4efb85f339
Sarel Lieberman <107625247+sarellieberman@users.noreply.github.com>
2022-11-23 18:51:58 +0200

    commit

--- ex_1/part_2/Main.cpp
+++ ex_1/part_2/Main.cpp
@@ -7,50 +7,60 @@
 using namespace std;
 
 /*
+Converts a string to double vectors.
+Checks in the procces whether the input is correct.
+If it is, returnes 0;
+*/
+int turnToVector(string stringInput, vector<double> v)
+{
+    stringstream s(stringInput);
+    string token;
+    int i;
+    double num;
+
+    while (s >> token)
+    {
+        for (i = 0; i < sizeof(token) - 1; i++)
+        {
+            if (!isdigit(word[i]))
+            {
+                return 1;
+            }
+            num = strtod(word.c_str(), 0);
+            v.push_back(num);
+        }
+    }
+    return 0;
+}
+
+/*
 Calculates the distance between 2 given vectors.
 Uses multiple methods of distance calculating.
 */
 int main()
 {
-    string vec1, vec2, word;
-    double num;
+    string stringInput1, stringInput2;
     vector<double> v1, v2;
+    int size, error;
 
-    getline(cin, vec1);
-    getline(cin, vec2);
+    getline(cin, stringInput1);
+    getline(cin, stringInput2);
 
-    turnToVecto(vec1, v1);
+    error = turnToVector(stringInput1, v1) + turnToVector(stringInput2, v2);
+    size = sizeof(v1);
 
-    if (!validInput)
+    cout << error << endl;
+
+    if (error || (size != sizeof(v2)))
     {
-        cout << "invalid input, goodbye" return 0;
+        cout << "invalid input, goodbye" << endl;
+        return 0;
     }
-    int size = sizeof(vectors[0]);
+
     cout << euclidean(v1, v2, size) << endl;
     cout << manhattan(v1, v2, size) << endl;
     cout << chebyshev(v1, v2, size) << endl;
     cout << canberra(v1, v2, size) << endl;
     cout << minkowski(v1, v2, size, 2) << endl;
     return 0;
-}
-
-/*
-Checks if the input is correct.
-If it is, converts the vectors to double arrays in the procces.
-*/
-int validInput(string vec1, string ves2)
-{
-    vectors[0] = nu
-}
-
-void turnToVector()
-{
-    stringstream v1(vec1);
-    while (v1 >> word)
-    {
-        if (!is digit(word[0]))
-        {
-        }
-    }
-    s(vec1);
 }
