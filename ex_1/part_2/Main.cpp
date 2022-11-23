f5acd8b56da8e3f3aa761002879c3c4af0b97a53
Sarel Lieberman <107625247+sarellieberman@users.noreply.github.com>
2022-11-23 13:12:07 +0200

    getting started

--- ex_1/part_2/Main.cpp
+++ ex_1/part_2/Main.cpp
@@ -1,1 +1,33 @@
-#
+#include <iostream>
+#include <string.h>
+#include "VecDistances.h"
+
+using namespace std;
+
+/*
+Calculates the distance between 2 given vectors.
+Uses multiple methods of distance calculating.
+*/
+void main()
+{
+    string vec1, vec2;
+    double ** vectors;
+
+    getline(cin, vec1);
+    getline(cin, vec2);
+    while (!validInput(vec1, vec2, vectors))
+    {
+        cout << "wrong input, type again\n";
+        getline(cin, vec1);
+        getline(cin, vec2);
+    }
+}
+
+/*
+Checks if the input is correct.
+If it is, calls a function that converts the vectors to double arrays.
+*/
+int validInput(string vec1, string ves2, double **vectors)
+{
+    
+}
