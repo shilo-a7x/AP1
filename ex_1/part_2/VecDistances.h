42aaddfadd1c842606ba5fc9e0becdf85d0715c1
Sarel Lieberman <107625247+sarellieberman@users.noreply.github.com>
2022-11-23 14:27:54 +0200

    update

--- ex_1/part_2/VecDistances.h
+++
@@ -1,8 +0,0 @@
-#include <cmath>
-
-using namespace std;
-double euclidean(double* vec1, double* vec2, int size);
-double manhattan(double* vec1, double* vec2, int size);
-double chebyshev(double *vec1, double *vec2, int size);
-double canberra(double *vec1, double *vec2, int size);
-double minkowski(double *vec1, double *vec2, int size, double r);
