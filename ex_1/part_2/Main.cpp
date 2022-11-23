082df5228179b2ddc4b693757d225001a0608eea
Sarel Lieberman <107625247+sarellieberman@users.noreply.github.com>
2022-11-23 14:28:04 +0200

    update

--- ex_1/part_2/Main.cpp
+++ ex_1/part_2/Main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-#include <string.h>
+#include <cstdlib>
+#include <sstream>
+#include <vector>
 #include "VecDistances.h"
 
 using namespace std;
@@ -8,26 +10,44 @@ using namespace std;
 Calculates the distance between 2 given vectors.
 Uses multiple methods of distance calculating.
 */
-void main()
+int main()
 {
-    string vec1, vec2;
-    double ** vectors;
+    string vec1, vec2, word;
+    double num;
+    vector<double> v1, v2;
 
     getline(cin, vec1);
     getline(cin, vec2);
-    while (!validInput(vec1, vec2, vectors))
-    {
-        cout << "wrong input, type again\n";
-        getline(cin, vec1);
-        getline(cin, vec2);
+    
+    stringstream v1(vec1);
+    while (v1 >> word){
+        if (!is digit(word[0])){
+
+        }
     }
+    s(vec1);
+
+    turnToVectors(vec1, vec2);
+
+    if (!validInput)
+    {
+        cout << "invalid input, goodbye" return 0;
+    }
+    int size = sizeof(vectors[0]);
+    cout << euclidean(vectors[0], vectors[1], size) << endl 
+    cout << manhattan(vectors[0], vectors[1], size) << endl 
+    cout << chebyshev(vectors[0], vectors[1], size) << endl 
+    cout << canberra(vectors[0], vectors[1], size) << endl 
+    cout << minkowski(vectors[0], vectors[1], size, 2) << endl return 0;
 }
 
 /*
 Checks if the input is correct.
-If it is, calls a function that converts the vectors to double arrays.
+If it is, converts the vectors to double arrays in the procces.
 */
-int validInput(string vec1, string ves2, double **vectors)
+int validInput(string vec1, string ves2){
+    vectors[0] = nu
+
+turnToVectors()
 {
-    
 }
