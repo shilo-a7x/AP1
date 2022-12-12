3ba6619a64c72202791cab3825a6f737899ab088
Shilo <shilo.avital@gmail.com>
2022-12-12 13:54:51 +0200

    fix lo-tove bugs

--- ex_2/Initializer.cpp
+++ ex_2/Initializer.cpp
@@ -1,21 +1,13 @@
 #include "Initializer.h"
 
-Initializer::Initializer(string type)
-{
-    this->disFunc = Distance(type);
-        string err = disFunc.getType;
-        if (err.compare("ERR"))
-        {
-            cout << "wrong distance input" << endl;
-            this->disFunc = nullptr;
-        }
-}
+Initializer::Initializer(string type) : disFunc(type) {}
 
 vector<Classifiable> Initializer::init(vector<Classifiable> vectors, vector<double> v)
 {
     int i;
-    for(i=0; i<vectors.size(); i++){
-        vectors[i].setDistance(disFunc.dis(v,vectors[i].getCoordinates()));
+    for (i = 0; i < vectors.size(); i++)
+    {
+        vectors[i].setDistance(disFunc.dis(v, vectors[i].getCoordinates()));
     }
     return vectors;
 }
