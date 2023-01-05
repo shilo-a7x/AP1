c6c37b9620629360bfdf900eac82ed66a134637d
Sarel Lieberman <107625247+sarellieberman@users.noreply.github.com>
2023-01-05 18:53:52 +0200

    commit

--- ex_3/server.cpp
+++ ex_3/server.cpp
@@ -11,17 +11,20 @@ The main function for lunching the server.
 */
 int main(int argc, char *argv[])
 {
-    //
+    // making sure the user sent a arguments.
     if (argc != 3)
     {
         cout << "Needs 3 valid arguments to run the server!" << endl;
         return 0;
     }
-    
-    Reader reader;
-    vector<vector<string>> data;
+
+    // if there's an exeption while initializing the server don't crash, just send an error massage and quit.
     try
     {
+        Reader reader;
+        vector<vector<string>> data;
+
+        //use the reader in order
         data = reader.readCSV(argv[1]);
         vector<Classifiable> classified = Classifiable::toVector(data, true);
         int port = atoi(argv[2]);
@@ -31,10 +34,11 @@ int main(int argc, char *argv[])
             throw runtime_error("failed to initialize socket\n");
         }
     }
+
+    // if there was a problem send an error massage and quit.
     catch (const exception &e)
     {
-        cout << "unable to start the server\n"
-             << endl;
+        cout << "unable to start the server\n" << endl;
         return 0;
     }
 
@@ -53,13 +57,14 @@ int main(int argc, char *argv[])
             if (is_number(coordinate))
             {
                 // tures a string to a double.
-                num = strtod(token.c_str(), 0);
+                num = strtod(coordinate.c_str(), 0);
 
                 // add the number to the vector.
                 vec.push_back(num);
                 continue;
             }
             DIS = coordinate;
+            break;
         }
         ss >> k;
         K = atoi(k);
