ce594cf7ae19f705b9fe2a0a08dc4511620b7ac3
Sarel Lieberman <107625247+sarellieberman@users.noreply.github.com>
2023-01-21 23:43:21 +0200

    now command 2 is done

---
+++ ex_4/changeSetsCommand.cpp
@@ -0,0 +1,49 @@
+#include "ChangeSetsCommand.h"
+
+void ChangeSetsCommand::execute() override
+    {
+        this->getIO()->write(this->getData()->getKnn()->detailString());
+        string input = this->getIO()->read();
+        if (input.empty())
+        {
+            return;
+        }
+        vector<string> param = Reader::split(input, ' ');
+        if (param.size() != 2)
+        {
+            this->getIO()->write("invalid value for K\ninvalid value for metric\n");
+            return;
+        }
+        int k, flag = 0;
+        try
+        {
+            k = stoi(param[0]);
+        }
+        catch (const invalid_argument &exception)
+        {
+            this->getIO()->write("invalid value for K\n");
+            flag = 1;
+        }
+        if (k < 0 && !flag)
+        {
+            this->getIO()->write("invalid value for K\n");
+            flag = 1;
+        }
+        Distance dist(param[1]);
+        if (!dist.getType().compare("ERR"))
+        {
+            this->getIO()->write("invalid value for metric\n");
+            return;
+        }
+        else
+        {
+            if (flag)
+            {
+                return;
+            }
+            else
+            {
+                this->getData()->getKnn()->setKNN(param[1], k);
+            }
+        }
+    }
--- ex_4/changeSetsCommand.h
+++ ex_4/changeSetsCommand.h
@@ -2,63 +2,17 @@
 #include "Reader.h"
 #include "Distance.h"
 
-#ifndef CLIENT_CHANGEALGOSETTINGS_H
-#define CLIENT_CHANGEALGOSETTINGS_H
+#ifndef CHANGEALGOSETTINGS_H
+#define CHANGEALGOSETTINGS_H
 
 using namespace std;
 
-class ChangeAlgoSettingsCommand : public Command
+class ChangeSetsCommand : public Command
 {
 public:
-    void execute() override
-    {
-        this->getIO()->write(this->getData()->getKnn()->detailString());
-        string input = this->getIO()->read();
-        if (input.empty())
-        {
-            return;
-        }
-        vector<string> param = Reader::split(input, ' ');
-        if (param.size() != 2)
-        {
-            this->getIO()->write("invalid value for K\ninvalid value for metric\n");
-            return;
-        }
-        int k, flag = 0;
-        try
-        {
-            k = stoi(param[0]);
-        }
-        catch (const invalid_argument &exception)
-        {
-            this->getIO()->write("invalid value for K\n");
-            flag = 1;
-        }
-        if (k < 0 && !flag)
-        {
-            this->getIO()->write("invalid value for K\n");
-            flag = 1;
-        }
-        Distance dist(param[1]);
-        if (!dist.getType().compare("ERR"))
-        {
-            this->getIO()->write("invalid value for metric\n");
-            return;
-        }
-        else
-        {
-            if (flag)
-            {
-                return;
-            }
-            else
-            {
-                this->getData()->getKnn()->setKNN(param[1], k);
-            }
-        }
-    }
+    void execute() override;
 
-    ChangeAlgoSettingsCommand(DefaultIO *io, KnnData *data) : Command("algorithm settings", io, data) {}
+    ChangeSetsCommand(DefaultIO *io, KnnData *data) : Command("algorithm settings", io, data) {}
 };
 
-#endif // CLIENT_CHANGEALGOSETTINGS_H
+#endif
