76abce20bc1d5e37bd8a00afb0b20839311da614
Sarel Lieberman <107625247+sarellieberman@users.noreply.github.com>
2023-01-22 13:27:04 +0200

    CLI and download command

---
+++ ex_4/DownloadResultsCommand.h
@@ -0,0 +1,19 @@
+#include "Command.h"
+
+#ifndef DOWNLOADCOMMAND_H
+#define DOWNLOADCOMMAND_H
+
+using namespace std;
+
+class DownloadResultsCommand : public Command
+{
+public:
+    void execute() override;
+
+    /**
+     * A constructor function.
+     */
+    DownloadResultsCommand(DefaultIO *io, KnnData *data) : Command("download results", io, data) {}
+};
+
+#endif
